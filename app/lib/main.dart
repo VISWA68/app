bf4a9df7007f7cd85d459ce55ec00c0bfa94606e
VISWA68 <viswavikas0607@gmail.com>
2025-08-04 21:52:09 +0530

    Added firebase to the project root

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:app/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'config/app_theme.dart';
 import 'providers/memory_provider.dart';
 import 'screens/home_screen.dart'; // We'll create this
 
-void main() {
-  // Make sure you initialize Firebase here
+void main()async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(); // Uncomment this after setup
+    
+  await Firebase.initializeApp(); 
 
   runApp(
     ChangeNotifierProvider(
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Our Little World',
       theme: appTheme,
-      home: SplashScreen(), // We'll create this
+      home: SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
